45c8f9362e7fd6d75b095db8c6c7210f6207815d
cormaal <clonedcormack@gmail.com>
2022-10-30 23:22:16 +0100

    Cleaned up redundant code

--- code/03_Getting_and_Cleaning_Data_Assignment.R
+++ code/03_Getting_and_Cleaning_Data_Assignment.R
@@ -2,29 +2,20 @@
 
 library(dplyr)
 
-
 feature_names <- read.table("./data/UCI_HAR_Dataset/features.txt")
 features <- feature_names$V2
 features <- gsub("^t", "time.", features)
 features <- gsub("^f", "freq.", features)
 
-
 subject_test <- read.table("./data/UCI_HAR_Dataset/test/subject_test.txt")
-colnames(subject_test) <- "subject"
-
 subject_train <- read.table("./data/UCI_HAR_Dataset/train/subject_train.txt")
-colnames(subject_train) <- "subject"
-
-subject <- rbind(subject_test, subject_train)
-
+subjects <- rbind(subject_test, subject_train)
+colnames(subjects) <- "subject"
 
 activity_test <- read.table("./data/UCI_HAR_Dataset/test/y_test.txt")
-colnames(activity_test) <- "activity"
-
 activity_train <- read.table("./data/UCI_HAR_Dataset/train/y_train.txt")
-colnames(activity_train) <- "activity"
-
 activities <- rbind(activity_test, activity_train)
+colnames(activities) <- "activity"
 
 activities$activity[activities$activity == 1] <- "walking"
 activities$activity[activities$activity == 2] <- "walking_upstairs"
@@ -33,20 +24,14 @@ activities$activity[activities$activity == 4] <- "sitting"
 activities$activity[activities$activity == 5] <- "standing"
 activities$activity[activities$activity == 6] <- "lying"
 
-
 test_data <- read.table("./data/UCI_HAR_Dataset/test/X_test.txt")
-colnames(test_data) <- features
-
 train_data <- read.table("./data/UCI_HAR_Dataset/train/X_train.txt")
-colnames(train_data) <- features
-
 all_data <- rbind(test_data, train_data)
+colnames(all_data) <- features
 
-
-complete_dataset <- bind_cols(subject, activities, all_data) %>%
+complete_dataset <- bind_cols(subjects, activities, all_data) %>%
         select(subject, activity, contains("mean()"), contains("std()"))
-        
-        
+   
 grouped_dataset <- group_by(complete_dataset, subject, activity) %>%
         summarise(across("time.BodyAcc-mean()-X":"freq.BodyBodyGyroJerkMag-std()", mean))
         
